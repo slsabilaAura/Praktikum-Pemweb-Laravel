f6e10f2a854033640bfaba80422ed3b55c21c07d
slsabilaAura <shalsabilaauraadiar@student.uns.ac.id>
2023-07-11 16:14:48 +0700

    terbaru

--- app/Http/Controllers/PermintaanController.php
+++ app/Http/Controllers/PermintaanController.php
@@ -6,6 +6,7 @@ use App\Models\Permintaan;
 
 use App\Models\Ruangan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -23,11 +24,6 @@ class PermintaanController extends Controller
         return view('request.form-request');
     }
 
-    // public function show($id)
-    // {
-    // $item = Ruangan::findOrFail($id);
-    // return view('request.form-request', compact('item'));
-    // }
 
     
 
@@ -63,17 +59,60 @@ class PermintaanController extends Controller
     return redirect('/ruangan')->withSuccess('Pendaftaran berhasil.');
 }
 
-
-public function daftarPermintaan()
+//USER DAFTAR PERMINTAAN
+public function daftarPermintaan(Request $request)
 {
-    $user_id = auth()->id();
-    $permintaan = Permintaan::where('user_id', $user_id)
-                            
-                            ->join('ruangan', 'request.ruangan_id', '=', 'ruangan.id')
-                            ->select('ruangan.nama_ruangan', 'request.tanggal_pinjam', 'request.tanggal_kembali', 'request.status')
-                            ->get();
+   
+    // $permintaan = Permintaan::with(['user', 'ruangan'])->get();
+
+    $search = $request->input('search');
+    $permintaan = Permintaan::with(['user', 'ruangan']);
+
+    if ($search) {
+        $permintaan->where(function ($query) use ($search) {
+            $query->whereHas('user', function ($userQuery) use ($search) {
+                $userQuery->where('name', 'LIKE', "%$search%");
+            })
+            ->orWhereHas('ruangan', function ($ruanganQuery) use ($search) {
+                $ruanganQuery->where('nama_ruangan', 'LIKE', "%$search%");
+            })
+            ->orWhere('tanggal_pinjam', 'LIKE', "%$search%")
+            ->orWhere('tanggal_kembali', 'LIKE', "%$search%")
+            ->orWhere('status', 'LIKE', "%$search%");
+        });
+    }
+
+    $permintaan = $permintaan->get();
 
     return view('request.daftar-permintaan', compact('permintaan'));
+}
+
+public function edit($id)
+{
+    $permintaan = Permintaan::findOrFail($id);
+    return view('request.edit-permintaan', compact('permintaan'));
+}
+
+public function update(Request $request, $id)
+{
+    $permintaan = Permintaan::findOrFail($id);
+    $permintaan->tanggal_pinjam = $request->input('tanggal_pinjam');
+    $permintaan->tanggal_kembali = $request->input('tanggal_kembali');
+    $permintaan->save();
+
+    return redirect()->route('daftar-permintaan')->with('success', 'Permintaan berhasil diupdate.');
+}
+public function destroy($id)
+{
+    $permintaan = Permintaan::findOrFail($id);
+
+    if (Gate::denies('delete-permintaan', $permintaan)) {
+        abort(403, 'Anda tidak memiliki izin untuk menghapus permintaan ini.');
+    }
+
+    $permintaan->delete();
+
+    return redirect()->route('daftar-permintaan')->with('success', 'Permintaan berhasil dihapus.');
 }
    
 }
